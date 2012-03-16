e74770dda0f0dbac9523937df8d4d38fa9171b71
KhanhNV <KhanhNV@KhanhNV-RPC>
2012-03-17 04:34:32 +0900

    +)Loading Icon feature 	- Use AsyncTask

--- android_fortest/src/net/knvz/android_fortest/Android_fortestActivity.java
+++ android_fortest/src/net/knvz/android_fortest/Android_fortestActivity.java
@@ -1,20 +1,22 @@
 package net.knvz.android_fortest;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.MotionEvent;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.ViewAnimator;
-import android.widget.ViewFlipper;
+import android.util.Log;
+import android.widget.ImageView;
 
 
 public class Android_fortestActivity extends Activity{
@@ -27,5 +29,15 @@ public class Android_fortestActivity extends Activity{
         rbV = new RibbonView(this);        
         setContentView(rbV);
         //setContentView(new EntryImgView(this, BitmapFactory.decodeResource(this.getResources(), R.drawable.img_test1), null));
-    }    
+        /*
+        ImageView iv = new ImageView(this);
+        try {
+			iv.setImageBitmap(downloadBitmap("http://1.bp.blogspot.com/-yIf0KriZdUg/TkAD6DXU15I/AAAAAAAAIZ4/mlcb3Yi0TDc/s1600/Desktop+wallpaper+Free+2.jpg"));
+		} catch (IOException e) {	
+			Log.d("debug", e.toString());			
+		}
+        setContentView(iv);
+        */
+   }
+ 
 }
