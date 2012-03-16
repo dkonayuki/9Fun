6e37767a678e054b2b57501f3788e32549d3648d
KhanhNV <KhanhNV@KhanhNV-RPC>
2012-03-17 04:56:09 +0900

    +)Add loading.gif +)Replace EntryImgView and RibbonView with the newest updated ones.

--- gagFUN/src/net/jstudio/gagfun/EntryImgView.java
+++ gagFUN/src/net/jstudio/gagfun/EntryImgView.java
@@ -1,42 +1,105 @@
 package net.jstudio.gagfun;
 
+import java.io.IOException;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.content.Context;
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
 
 public class EntryImgView extends View {
+	private static final String sLoadingFileName = "loading.gif";
 	private Bitmap m_bitmap;
 	private ScaleGestureDetector _scaleDetector;
 	private GestureDetector _gestureDetector;
 	private TransformRect r_img;
 	private RibbonView m_rbV;
+	private Movie mv_Loading;	
+	private boolean isLoading;
+	private long lStartImageLoading = 0;
+	private DownloadImageTask dlTask;
 	
 	public EntryImgView(Context context) {
 		super(context);
 	}
 	
-	public EntryImgView(Context context, Bitmap bmp, RibbonView rbV){
+	public EntryImgView(Context context, String url, RibbonView rbV){
 		this(context);
-		m_bitmap = bmp;
+		//m_bitmap = bmp;
+		//Load Loading image
+		try {
+			mv_Loading = Movie.decodeStream(context.getAssets().open(sLoadingFileName));
+		} catch (IOException e) {
+		}
 		m_rbV = rbV;
+		isLoading = true;
+		//Start Download Image
+		dlTask = new DownloadImageTask();
+		dlTask.execute(url);
+		//
 		_scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
 		_gestureDetector = new GestureDetector(context, new GestureListener());
-		_gestureDetector.setOnDoubleTapListener(new DoubleTapListener());
+		_gestureDetector.setOnDoubleTapListener(new DoubleTapListener());		
 	}
 
+	
+	  
+	private Bitmap downloadBitmap(String url) throws IOException {
+		HttpUriRequest request = new HttpGet(url.toString());
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpResponse response = httpClient.execute(request);
+
+		StatusLine statusLine = response.getStatusLine();
+		int statusCode = statusLine.getStatusCode();
+		if (statusCode == 200) {
+			HttpEntity entity = response.getEntity();
+			byte[] bytes = EntityUtils.toByteArray(entity);
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0,
+					bytes.length);
+			return bitmap;
+		} else {
+			throw new IOException("Download failed, HTTP response code "
+					+ statusCode + " - " + statusLine.getReasonPhrase());
+		}
+	}
+		
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawBitmap(m_bitmap, null, r_img.getRect(), null);
+		if(isLoading){
+			canvas.drawColor(Color.TRANSPARENT);
+			long now = SystemClock.uptimeMillis();
+			if(lStartImageLoading == 0)
+				lStartImageLoading = now;
+			int relTime = (int)(now - lStartImageLoading) % mv_Loading.duration();
+			mv_Loading.setTime(relTime);			
+			mv_Loading.draw(canvas, (this.getWidth() - mv_Loading.width())/2, (this.getHeight() - mv_Loading.height())/2);
+			invalidate();	
+		}else if(r_img != null)
+			canvas.drawBitmap(m_bitmap, null, r_img.getRect(), null);
 	}
-
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {		
-		float scaleW = m_bitmap.getWidth()/w; //No handle for scaleW < 1;
+	
+	private void FixImageSize(int w, int h){
+		float scaleW = (float)m_bitmap.getWidth()/w; //No handle for scaleW < 1;
 		
 		int left, top, width, height;		
 		width = w;
@@ -49,6 +112,13 @@ public class EntryImgView extends View {
 		r_img = new TransformRect(left, top, width, height, h);
 		r_img.setMaxScale(3.f);
 		r_img.setMinScale(1.f);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		if(!isLoading){
+			FixImageSize(w, h);
+		}
 		super.onSizeChanged(w, h, oldw, oldh);
 	}
 
@@ -59,24 +129,48 @@ public class EntryImgView extends View {
 		return true;		
 	}
 	
+	private class DownloadImageTask extends AsyncTask<String, Void, Void>{
+
+		@Override
+		protected Void doInBackground(String... params) {
+			//Delete later
+			try {
+				m_bitmap = downloadBitmap(params[0]);
+			} catch (IOException e) {				
+			}
+			//
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {			
+			isLoading = false;
+			int w = getWidth(), h = getHeight();
+			FixImageSize(w, h);			
+			invalidate();
+		}
+		
+		
+	}
+	
 	
 	private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
 
 		@Override
 		public boolean onScale(ScaleGestureDetector detector) {
-			float _scale = detector.getScaleFactor();
-			r_img.Scale((int)detector.getFocusX(), (int)detector.getFocusY()
-					, _scale);
-			//r_img.FixScale();
-			invalidate();
+			if(!isLoading){
+				float _scale = detector.getScaleFactor();
+				r_img.Scale((int)detector.getFocusX(), (int)detector.getFocusY()
+						, _scale);
+				//r_img.FixScale();
+				invalidate();
+			}
 			return true;
 		}
 
 		@Override
 		public void onScaleEnd(ScaleGestureDetector detector) {			
-			super.onScaleEnd(detector);
-			//r_img.FixScale();
-			//invalidate();			
+			super.onScaleEnd(detector);			
 		}
 		
 	}
@@ -84,13 +178,13 @@ public class EntryImgView extends View {
 	private class DoubleTapListener implements GestureDetector.OnDoubleTapListener{
 
 		public boolean onDoubleTap(MotionEvent e) {
-			
-			if(r_img.getScaled() != 1.f)
-				r_img.Reset();				
-			else
-				r_img.Scale((int)e.getX(), (int)e.getY(), 2.f);
-			invalidate();
-			
+			if(!isLoading){
+				if(r_img.getScaled() != 1.f)
+					r_img.Reset();				
+				else
+					r_img.Scale((int)e.getX(), (int)e.getY(), 2.f);
+				invalidate();
+			}
 			return true;
 		}
 
@@ -111,12 +205,19 @@ public class EntryImgView extends View {
 		}
 
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-				float velocityY) {			
-			if(velocityX < 0 && r_img.canGoNext()){
-				m_rbV.goNext();				
-			}else if (velocityX >= 0 && r_img.canGoPrevious()){
-				m_rbV.goPrevious();				
-			}			
+				float velocityY) {
+			if(!isLoading){
+				if(velocityX < 0 && r_img.canGoNext()){
+					m_rbV.goNext();				
+				}else if (velocityX >= 0 && r_img.canGoPrevious()){
+					m_rbV.goPrevious();				
+				}			
+			}else{
+				if(velocityX < 0)
+					m_rbV.goNext();
+				else
+					m_rbV.goPrevious();
+			}
 			return true;
 		}
 
@@ -125,8 +226,10 @@ public class EntryImgView extends View {
 
 		public boolean onScroll(MotionEvent e1, MotionEvent e2,
 				float distanceX, float distanceY) {
-			r_img.Translate(-(int)distanceX, -(int)distanceY);
-			invalidate();
+			if(!isLoading){
+				r_img.Translate(-(int)distanceX, -(int)distanceY);
+				invalidate();
+			}
 			return true;
 		}
 		public void onShowPress(MotionEvent e) {}
--- gagFUN/src/net/jstudio/gagfun/RibbonView.java
+++ gagFUN/src/net/jstudio/gagfun/RibbonView.java
@@ -1,22 +1,23 @@
 package net.jstudio.gagfun;
 
 import android.content.Context;
+//import android.graphics.BitmapFactory;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ViewAnimator;
 
-public class RibbonView extends ViewAnimator {
-	//private final static int MAX_VIEW_CHILD = 5;
+public class RibbonView extends ViewAnimator {	
 	private Animation 	anim_InFromLeft,
 						anim_InFromRight,
 						anim_OutToLeft,
-						anim_OutToRight;
+						anim_OutToRight;	
 	
 	public RibbonView(Context ct) {
-		super(ct);
+		super(ct);		
+		setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
 		loadAnimation();
 		/*
-		setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));		
 		EntryImgView entry1 = new EntryImgView(ct, BitmapFactory.decodeResource(this.getResources(), R.drawable.img_test1), this);
 		EntryImgView entry2 = new EntryImgView(ct, BitmapFactory.decodeResource(this.getResources(), R.drawable.img_test2), this);
 		EntryImgView entry3 = new EntryImgView(ct, BitmapFactory.decodeResource(this.getResources(), R.drawable.img_test3), this);
@@ -27,9 +28,14 @@ public class RibbonView extends ViewAnimator {
 		addView(entry3);
 		addView(entry4);
 		addView(entry5);
-		*/		
-	}
-	
+		*/
+		EntryImgView entry1 = new EntryImgView(ct, "http://1.bp.blogspot.com/-yIf0KriZdUg/TkAD6DXU15I/AAAAAAAAIZ4/mlcb3Yi0TDc/s1600/Desktop+wallpaper+Free+2.jpg", this);
+		EntryImgView entry2 = new EntryImgView(ct, "http://4.bp.blogspot.com/-THHiXm8Fd8c/Tl8wXRailjI/AAAAAAAAHCY/k9RlVDfOjtA/s1600/free+abstract+backgrounds-6.jpg", this);
+		addView(entry1);
+		addView(entry2);
+		//addView(entry1);
+	}	
+
 	private void loadAnimation(){
 		Context ct = this.getContext();
 		anim_InFromLeft = AnimationUtils.loadAnimation(ct, R.anim.infromleft);
