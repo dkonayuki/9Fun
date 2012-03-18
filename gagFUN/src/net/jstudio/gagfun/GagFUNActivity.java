1f4b6a5f7be737199bc9a2ee7c7faf23ec51173e
UndeadKnight <UndeadKnight@192.168.11.5>
2012-03-19 04:08:45 +0900

    set fullscreen

--- gagFUN/src/net/jstudio/gagfun/GagFUNActivity.java
+++ gagFUN/src/net/jstudio/gagfun/GagFUNActivity.java
@@ -2,6 +2,8 @@ package net.jstudio.gagfun;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GagFUNActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,7 +11,9 @@ public class GagFUNActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        rbV = new RibbonView(this);        
+        rbV = new RibbonView(this);    
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(rbV);
     }
 }
