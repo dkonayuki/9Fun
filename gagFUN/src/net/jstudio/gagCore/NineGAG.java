99653f620f598cd70e6f9b50afe643e3f9679ac7
khanhnv <khanhnv90@gmail.com>
2012-04-13 23:38:58 +0900

    +)Fix no internet bug

--- gagFUN/src/net/jstudio/gagCore/NineGAG.java
+++ gagFUN/src/net/jstudio/gagCore/NineGAG.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
